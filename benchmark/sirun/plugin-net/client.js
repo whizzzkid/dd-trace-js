061c0b7fb3a732324e15da4f4890d6eeae5708c6
Bryan English <bryan.english@datadoghq.com>
2021-04-12 17:02:49 -0400

    fix

--- benchmark/sirun/plugin-net/client.js
+++ benchmark/sirun/plugin-net/client.js
@@ -2,17 +2,25 @@
 
 const net = require('net')
 
-let connectionsMade = 0
+if (process.env.SET_PID === 'client') {
+  const fs = require('fs')
+  fs.writeFileSync('client.pid', '' + process.pid)
+}
 
+let connectionsMade = 0
 function run () {
   const client = net.connect(process.env.PORT, () => {
     client.write('hello')
     client.on('data', () => {
-      client.end()
-      if (++connectionsMade !== 10000) {
+      client.end(() => {
+        if (++connectionsMade === 10000 && process.env.SET_PID !== 'client') {
+          process.exit()
+        }
         run()
-      }
+      })
     })
+  }).on('error', () => {
+    setTimeout(run, 100)
   })
 }
 run()
--- benchmark/sirun/plugin-net/server.js
+++ benchmark/sirun/plugin-net/server.js
@@ -2,10 +2,20 @@
 
 const net = require('net')
 
+if (process.env.SET_PID === 'server') {
+  const fs = require('fs')
+  fs.writeFileSync('server.pid', '' + process.pid)
+}
+
 let connectionsMade = 0
-const server = net.createServer(c => {
-  if (++connectionsMade === 10000) {
-    c.on('end', () => server.close())
-  }
-  c.pipe(c)
+net.createServer(c => {
+  c.on('data', d => {
+    c.write(d)
+
+    if (++connectionsMade === 10000 && process.env.SET_PID !== 'server') {
+      setImmediate(() => {
+        process.exit()
+      })
+    }
+  })
 }).listen(process.env.PORT)
